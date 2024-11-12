ebbf0f6f8ad5ca75fc1ca649e45b2158b407c44c
Manvith-003 <manvithpoojary63@gmail.com>
2024-11-12 10:21:53 +0530

    add vehicle backend

---
+++ admin/add_vehicle_backend.php
@@ -0,0 +1,30 @@
+<?php 
+        include('../db.php');
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+            $vehicle_type=$_POST['vehicleType'];
+            $v_regno=$_POST['registerNumber'];
+            $v_insurance_no= $_POST['insuranceNumber'];
+            $insurance_valid= $_POST['insuranceValidTill'];
+            $emission_valid= $_POST['emissionValidTill'];
+
+            
+            $query="INSERT INTO `add_vehicle`(`vehicle_type`,`v_reg_no`,`v_insurance_no`,`v_insurance_valid`,`v_emission_valid`)VALUES('$vehicle_type','$v_regno','$v_insurance_no','$insurance_valid','$emission_valid')";
+           
+            if($conn->query($query)){
+                echo"<script>alert('inserted sucessfully');</script>";
+                echo"<script>location.href='add_vehicle.php'</script>";
+            }else{
+                echo"<script>alert('Not inserted');</script>";
+                echo"<script>location.href='add_vehicle.php'</script>";
+                }
+            }
+                
+            //$con->query($query);
+       
+         $conn->close();
+
+        // // End output buffering and flush the buffer
+        // ob_end_flush();
+    ?>
