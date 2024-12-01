979af58ea978d79ec1c7b196800d0c83d77c7b3c
Karthik <karthikykr16@gmail.com>
2024-12-01 20:59:49 +0530

    style add vehicle form

--- admin/add_vehicle.php
+++ admin/add_vehicle.php
@@ -1,7 +1,7 @@
 
 <?php include('../header.php');?>
     <title>Transport</title>
-    <link rel="stylesheet" href="../styles/add_vehicle.css"> <!-- Your custom CSS file -->
+    <link rel="stylesheet" href="../styles/add_vehicle1.css"> <!-- Your custom CSS file -->
 
     <!-- Main Content Area -->
     <main class="main-content">
@@ -11,33 +11,53 @@
 <body>
 
 <!-- Form to Add Vehicle -->
-<div class="add_vehicles" >
+<div class="form-container" >
     <h2>Add Vehicle</h2><br>
     <form id="vehicleForm" action="add_vehicle_backend.php" method="POST" >  <!--redirect to add_vehicle_backend --> 
+         
+                <div class="row">
+                    <div class="form-group">
+                        <label for="vehicle">Type of Vehicle:</label>
+                        <select id="vehicle" name="vehicle" required>
+                            <option value="Car">Car</option>
+                            <option value="Bus">Bus</option>
+                            <option value="Tempo Traveller">Tempo Traveller</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="registerNumber">Enter Vehicle Register Number:</label>
+                        <input type="text" id="registerNumber" name="registerNumber" class="reg_input" required>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="form-group">
+                        <label for="insuranceNumber">Enter Insurance Number:</label>
+                        <input type="text" id="insuranceNumber" name="insuranceNumber" class="lic_input" required>
+                    </div>
+                <div class="form-group">
+                    <label for="insuranceValidTill">Insurance Valid Till:</label>
+                    <input type="date" id="insuranceValidTill" name="insuranceValidTill" required>
+                </div>
+                </div>
+
             
-        <label for="vehicles">Select Vehicle Type:</label>
-        <select id="vehicles" name="vehicleType" class="vehicle" required>
-            <option value="" disabled selected>Select a vehicle</option>
-            <option value="bus">Bus</option>
-            <option value="car">Car</option>
-            <option value="truck">Truck</option>
-        </select><br><br>
+                <div class="form-group">
+                    <label for="emissionValidTill">Emission Valid Till:</label>
+                    <input type="date" id="emissionValidTill" name="emissionValidTill"  required>
+                </div>
+                <div class="btn-con">
+                    <button type="submit"  class="btnAdd">Add Vehicle</button>
+                </div>
 
-        <label for="registerNumber">Enter Vehicle Register Number:</label>
-        <input type="text" id="registerNumber" name="registerNumber" class="reg_input" required><br><br>
-
-        <label for="insuranceNumber">Enter Insurance Number:</label>
-        <input type="text" id="insuranceNumber" name="insuranceNumber" class="lic_input" required><br><br>
-
-        <label for="insuranceValidTill">Insurance Valid Till:</label>
-        <input type="date" id="insuranceValidTill" name="insuranceValidTill" required><br><br>
-
-        <label for="emissionValidTill">Emission Valid Till:</label>
-        <input type="date" id="emissionValidTill" name="emissionValidTill"  required><br><br>
-
-        <input type="submit" value="Add Vehicle" class="btnAdd"><br><br>
+        
     </form>
    
 </div>
 </body>
 </html>
+
+
+
+                
